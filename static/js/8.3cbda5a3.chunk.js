ee0235c4bf65d320da8d0d93d62c5c2d0fc41697
Gundam Cryptos <gundamcryptos@gmail.com>
2022-08-18 21:30:58 -0500

    Fix

--- static/js/8.3cbda5a3.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonppancake-frontend"]=this["webpackJsonppancake-frontend"]||[]).push([[8],{966:function(n,p){}}]);
-//# sourceMappingURL=8.3cbda5a3.chunk.js.map
--- static/js/8.ab6fe5a1.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonppancake-frontend"]=this["webpackJsonppancake-frontend"]||[]).push([[8],{965:function(n,p){}}]);
-//# sourceMappingURL=8.ab6fe5a1.chunk.js.map
